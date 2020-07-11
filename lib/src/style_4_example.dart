303a0ef65e368060657825bf921b8ab560c64ab5
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-07-11 21:49:27 +0100

    Added style 4

--- lib/src/style_4_example.dart
+++ lib/src/style_4_example.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-class Style4Example extends StatelessWidget {
+class Style4Example extends StatefulWidget {
+  @override
+  _Style4ExampleState createState() => _Style4ExampleState();
+}
+
+class _Style4ExampleState extends State<Style4Example> {
   @override
   Widget build(BuildContext context) {
     return Container();
