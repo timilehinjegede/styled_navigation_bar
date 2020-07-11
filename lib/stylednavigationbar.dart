5da9e3da447a6ef64a8f2159abaa296aea255f45
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-07-11 21:48:29 +0100

    Removed tests

--- lib/stylednavigationbar.dart
+++ lib/stylednavigationbar.dart
@@ -1,8 +1,3 @@
 library stylednavigationbar;
 
 export 'package:stylednavigationbar/src/rounded_bottom_nav.dart';
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
