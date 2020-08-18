abd480b427b0f75faedeb5efcd3db5b72df9f762
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-08-18 01:52:42 +0100

    updates: started style 2

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:example/styles_examples/style1_example.dart';
+import 'package:example/styles_examples/style2_example.dart';
 import 'package:flutter/material.dart';
 import 'package:stylednavigationbar/stylednavigationbar.dart';
 
@@ -7,16 +7,29 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
         primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Style1Example(
-        title: 'Style1 Example',
+      home: Style2Example(
+        title: 'Style2 Example',
       ),
     );
   }
