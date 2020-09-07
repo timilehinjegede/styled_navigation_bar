b7b2d531643476a450723bde777b6d0064838004
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-09-07 21:03:35 +0100

    style 8 started

--- lib/stylednavigationbar.dart
+++ lib/stylednavigationbar.dart
@@ -3,4 +3,4 @@ library stylednavigationbar;
 export 'package:stylednavigationbar/src/style1_bottom_nav.dart';
 export 'package:stylednavigationbar/src/style2_bottom_nav.dart';
 export 'package:stylednavigationbar/src/style4_bottom_nav.dart';
-export 'package:stylednavigationbar/src/style_7_bottom_nav.dart';
+export 'package:stylednavigationbar/src/style7_bottom_nav.dart';
