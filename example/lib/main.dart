12b6a58f37ceea2115c86a056c1db69c6d0ca0d4
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-09-07 20:27:14 +0100

    fixed dart analysis problems

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:example/styles_examples/style2_example.dart';
 import 'package:example/styles_examples/style7_example.dart';
 import 'package:flutter/material.dart';
-import 'package:stylednavigationbar/stylednavigationbar.dart';
 
 void main() {
   runApp(MyApp());
--- test/stylednavigationbar_test.dart
+++ test/stylednavigationbar_test.dart
@@ -1,6 +1,3 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:stylednavigationbar/stylednavigationbar.dart';
 
 void main() {
 //  test('adds one to input values', () {
